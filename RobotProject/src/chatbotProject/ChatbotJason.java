42817adb00ad2cbbc934329e33bf5b72bf6635fc
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-03 11:44:01 -0400

    Jason!

---
+++ RobotProject/src/chatbotProject/ChatbotJason.java
@@ -0,0 +1,67 @@
+package chatbotProject;
+
+public class ChatbotJason implements Topic {
+	private String[]keywords;
+	private String goodbyeWord;
+	private String secretWord;
+	private boolean chatting;
+	
+	public ChatbotJason() {
+		String[] temp = {"food","entertainment","internet","videogames"};
+		keywords = temp;
+		goodbyeWord = "bye";
+		secretWord = "pug";
+	}
+	
+	public boolean isTriggered(String response) {
+		for(int i=0;i<keywords.length;i++) {
+			if (ChatbotMain.findKeyword(response, keywords[i],0)>=0){
+			return true;	
+			}
+		}
+		return false;
+	}
+	public void startChatting(String response) {
+		ChatbotMain.print("Hey! It sounds like you and I have a common interest! Let's talk some more!");
+		chatting = true;
+		while(chatting) {
+			response = ChatbotMain.getInput();
+			if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
+				chatting = false;
+				ChatbotMain.chatbot.startTalking();
+			} else if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
+				ChatbotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now.");	
+			}else {
+				ChatbotMain.print("Huh. I don't really get you. Tell me something else?");
+			}
+		}
+	}
+	public void convincePerson(String response) {
+		boolean convinced = ChatbotMain.findKeyword(response, "yes", 0) == -1;
+		int counter = 0;
+		while(!convinced) {
+			if (counter == 0) {
+				ChatbotMain.print("Are you sure? There is a 10% off deal!");
+				counter++;
+			}
+			else if(counter == 1) {
+				ChatbotMain.print("Why!?!");
+				counter++;
+			}
+			else if (counter == 2) {
+				ChatbotMain.print("Will a 50% discount change your mind?");
+				counter++;
+			}
+			else if (counter == 3) {
+				convinced = true;
+			} 
+		}
+		if (counter == 3) {
+			flame();
+		}
+		
+	}
+	public void flame() {
+		ChatbotMain.print("You think your cool!?!");
+	}
+}
