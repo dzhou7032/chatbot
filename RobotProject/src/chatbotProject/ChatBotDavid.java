36c2bab46ffbeec17329aa3bfd80b510185cffe2
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-10-05 11:40:15 -0400

    Im so cool

--- RobotProject/src/chatbotProject/ChatBotDavid.java
+++ RobotProject/src/chatbotProject/ChatBotDavid.java
@@ -33,15 +33,38 @@ public class ChatBotDavid implements Topic {
 	public void startChatting(String response, int happiness) {
 		ChatbotMain.print("Hey! You know what would be fun? If we played a guessing game!");
 		chatting = true;
+		int chatcounter = 0;
 		while(chatting) {
 			response = ChatbotMain.getInput();
+			if (chatcounter == 0) {
 			for (int i =0;i<4; i++) {
 				if (ChatbotMain.findKeyword(response, agreementWord[i], 0) >= 0) {
-					ChatbotMain.print("Sweet, let's play guess my clothing!");
+					if (happiness== 2) {
+					ChatbotMain.print("I knew you would want to! Let's have some fun playing Guess my Clothing!");
 					playingGame = true;
 					startPlaying(response);
 					return;
-				}}
+					}
+					if (happiness == 1) {
+						ChatbotMain.print("Sweet, let's play Guess my Clothing!");
+						playingGame = true;
+						startPlaying(response);
+						return;
+					}
+					if (happiness == 0) {
+						ChatbotMain.print("Althought I may not like you, I like playing Guess my Clothing.");
+						playingGame = true;
+						startPlaying(response);
+						return;
+					}
+				}
+				else {
+					ChatbotMain.print("Fine, I didn't want to play Guess my Clothing with you anyways! What do you want to talk about?");
+					ChatbotMain.chatbot.decreaseHappiness();
+				}
+				}
+			chatcounter++;
+			}
 			 if(ChatbotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
 				chatting = false;
 				ChatbotMain.chatbot.startTalking();
@@ -53,26 +76,69 @@ public class ChatBotDavid implements Topic {
 		}
 	}
 	public void startPlaying(String response) {
+		String theAnswer;
 		ChatbotMain.print("The rules of guess my clothing are simple, I think of a specific piece of clothing and you guess what I'm thinking based off of the hints I give you");
 		int randomNum = (int)(Math.random() * 3);
+		System.out.println(randomNum);
 		if (randomNum == 0) {
-			String theAnswer = answers[0];
+			theAnswer = answers[0];
 		}
-		if (randomNum == 1) {
-			String theAnswer = answers[1];
+		else if (randomNum == 1) {
+			theAnswer = answers[1];
 		}
-		if (randomNum == 2) {
-			String theAnswer = answers[2];
+		else if (randomNum == 2) {
+			theAnswer = answers[2];
 		}
 		else {
-			String theAnswer = answers[3];
+			theAnswer = answers[3];
 		}
+		int counter = 0;
 		while (playingGame) {
 			response = ChatbotMain.getInput();
-			
-			
+			if (ChatbotMain.findKeyword(response, theAnswer , 0)>=0) {
+				counter++;
+				ChatbotMain.print("Wow, you found it within "+counter+" guesses!!");
+				ChatbotMain.chatbot.increaseHappiness();
+				startChatting(response, ChatbotMain.chatbot.getHappiness());
+				playingGame = false;
+			}
+			else if (!(ChatbotMain.findKeyword(response, theAnswer , 0)>=0)) {
+				counter++;
+				if((theAnswer.equals("sweater")||theAnswer.equals("shirt"))&&counter==1) {
+					ChatbotMain.print("This is worn on the upperbody");
+				}
+				else if((theAnswer.equals("sweatpants")||theAnswer.equals("shorts"))&&counter==1) {
+					ChatbotMain.print("This is worn on the lowerbody");
+				}
+				else if (theAnswer.equals("sweatpants")&&counter==2){
+					ChatbotMain.print("This keeps you very warm, yet you can work out in it");
+				}
+				else if ((theAnswer.equals("shorts"))&&counter==2) {
+					ChatbotMain.print("This keeps you cool and loose");
+				}
+				else if (theAnswer.equals("sweater")&&counter==2){
+					ChatbotMain.print("This is worn generally in the winter to keep you warm");
+				}
+				else if (theAnswer.equals("shirt")&&counter==2) {
+					ChatbotMain.print("This is worn generally in the summer to keep you cool");
+				}
+				else if (theAnswer.equals("sweatpants")&&counter==3) {
+					ChatbotMain.print("It is composed of two stand alone English words");
+				}
+				else if (theAnswer.equals("shorts")&&counter==3){
+					ChatbotMain.print("What's the opposite of long?");
+				}
+				else if (theAnswer.equals("sweater")&&counter==3) {
+					ChatbotMain.print("This word rhymes with weather");
+				}
+				else if (theAnswer.equals("shirt")&&counter==3) {
+					ChatbotMain.print("This word rhymes with hurt");
+				}
+				else {
+					ChatbotMain.print("I have already given you all the hints you need");
+				}
+			}
 		}
 		
 	}
 }
-
